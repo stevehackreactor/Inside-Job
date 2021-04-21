13b9d7ba8db53aae7891494179c2ecd91829c819
Steve <steve.gackstetter@gmail.com>
2021-04-21 00:07:59 -0500

    Created handle home function

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 
 const Sidebar = () => {
+
+  const handleHome = () => {
+    console.log('home clicked')
+  }
   return (
     <div className="sidebar">
-      <span>
+      <span onClick={ handleHome }>
         Home
       </span>
       <span>
